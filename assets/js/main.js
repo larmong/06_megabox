a68db9d48ef900d1cf102e654190d9a9df0910c2
LEEAREUM <larmong@naver.com>
2020-11-09 15:59:32 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 // main_js
+
+// slider(swiper)
+var mySwiper = new Swiper ('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 5000,
+    },
+})
